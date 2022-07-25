e98a4e5dfe0e1b868e51da2b9c49097c3cc43e38
bindura123 <binduraqa@gmail.com>
2022-07-25 15:43:22 +0530

    commit five

--- src/main/java/com/command/CommandClass.java
+++ src/main/java/com/command/CommandClass.java
@@ -5,6 +5,7 @@ public class CommandClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git Commands ");
 		System.out.println("Hello Git Commands1 ");
+		System.out.println("Hello Git Commands2 ");
 	}
 
 }
